7be572766a9b557c495a044093773f3d7f0566bb
Kenneth Spatta <kenyounot@gmail.com>
2019-10-28 18:58:15 -0400

    added login view and form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    
+    redirect "/login"
   end
 
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,12 @@
+class SessionsController < ApplicationController
+    get '/login' do
+        erb :"/sessions/login"
+    end
+
+    post '/login' do
+        
+    end
+
+
+
+end
